292f1b8e8cf02b546e9990d9ff899b3a983efec3
ToikaP <lucasperezgracia@gmail.com>
2019-06-10 00:22:23 -0300

    Primer Comit wacho

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/usuario', (req, res) => res.json('get Usuario'));
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    if (body.nombre === undefined){
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    }
+    res.json({
+        persona: body
+    })
+});
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', (req, res) => res.json('delete Usuario'));
+
+app.listen(3000, () => console.log('Escuchando puerto: ', process.env.PORT))
